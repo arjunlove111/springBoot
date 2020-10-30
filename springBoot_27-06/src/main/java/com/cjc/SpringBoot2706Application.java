8d06365457f377e6150e3446ccac646558bafc56
COMPUTER WORLD <COMPUTER WORLD@ThisPC>
2020-10-30 16:45:08 +0530

    developed by arjunn

---
+++ springBoot_27-06/src/main/java/com/cjc/SpringBoot2706Application.java
@@ -0,0 +1,15 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2706Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		SpringApplication.run(SpringBoot2706Application.class, args);
+	}
+
+}
---
+++ springBoot_27-06/src/test/java/com/cjc/SpringBoot2706ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2706ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
